cf226a20c238eb66c1fdc50f62999a37af613a9a
Sergio Conde <skgsergio@gmail.com>
2023-08-31 23:07:27 +0200

    pipx and update

--- oh-my-zsh-custom/config.sh
+++ oh-my-zsh-custom/config.sh
@@ -22,6 +22,10 @@ if [[ -d "$HOME/bin" ]]; then
     export PATH="$HOME/bin:$PATH"
 fi
 
+if [[ -d "$HOME/.local/bin" ]]; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
 if command -v podman &> /dev/null; then
     export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
 fi
