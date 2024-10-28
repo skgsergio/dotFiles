ef82a42bf3257cc25be66f43e21c15f29f5d781e
Sergio Conde <skgsergio@gmail.com>
2024-10-28 12:49:21 +0100

    Remove DOCKER_HOST var when using docker

--- oh-my-zsh-custom/config.sh
+++ oh-my-zsh-custom/config.sh
@@ -30,8 +30,6 @@ fi
 
 if command -v podman &> /dev/null; then
     export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
-elif command -v docker &> /dev/null; then
-    export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/docker.sock"
 fi
 
 if command -v batcat &> /dev/null; then
